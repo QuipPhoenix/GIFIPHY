e17a66adc16f043745f786df3ae04f33cbeb2cba
QuipPhoenix <manjot0998@gmail.com>
2020-11-18 11:17:41 +0530

    web assembly and ffmpeg used to make gif

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
 
   const [ready, setReady]  = useState(false);
   const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
 
   const load = async () => {
     await ffmpeg.load();
@@ -19,13 +20,36 @@ function App() {
     load();
   }, [])
   
+
+    const convertToGif = async () => {
+    //write video file to memory remains still before is refresh
+    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
+
+    //ffmpeg command
+    // i for input t for lenght ss for starting point f for format 
+    await ffmpeg.run("-i", 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif')
+
+    // Read the Result
+
+    const data = ffmpeg.FS('readFile', 'out.gif');
+
+    // create URL Blob is raw binary 
+    const url = URL.createObjectURL(new Blob([data.buffer], {type: 'image/gif'}));
+    setGif(url)
+
+  }
+
+
   return ready ? (
     <div className="App">
       {video && <video 
                     controls
                     width = "250"
-                    src = {URL.createObjectURL()} ></video>}
+                    src = {URL.createObjectURL(video)} ></video>}
       <VideoInput setVideo = {setVideo} />
+      <h3>Result</h3>
+      <button onClick = {convertToGif}>Convert</button>
+      {gif && <img src = {gif} width = "250"/>} 
       
     </div>
   ):
