35467b0a7c52e851df6b36a6a935983726549b84
QuipPhoenix <manjot0998@gmail.com>
2020-11-18 10:02:38 +0530

    FFmpeg is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-
+// starting point for working with ffmpeg library
+import { createFFmpeg, fetchFile} from '@ffmpeg/ffmpeg';
+import { use } from 'chai';
+// log is set true to see what is happening in background 
+const ffmpeg = createFFmpeg({log : true});
 function App() {
+
+  const [ready, setReady]  = useState(false);
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  }
+
+  useEffect(() => {
+    load();
+  }, [])
   
   return (
     <div className="App">
